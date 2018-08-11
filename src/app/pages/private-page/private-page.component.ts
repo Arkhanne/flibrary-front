2aaea5bc54635f7660eb96b9394b9009dd917e07
Edu Benito <eduardbenito@icloud.com>
2018-08-11 19:59:52 +0200

    initial commit

---
+++ src/app/pages/private-page/private-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-private-page',
+  templateUrl: './private-page.component.html',
+  styleUrls: ['./private-page.component.css']
+})
+export class PrivatePageComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
