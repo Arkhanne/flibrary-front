e3249b3e4d65d6036edd0c4c0ce1620b927385c0
Edu Benito <eduardbenito@icloud.com>
2018-08-14 10:27:54 +0200

    basic home / sign up / login / private pages navigation with user auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 import { PrivatePageComponent } from './pages/private-page/private-page.component';
 import { AuthService } from './services/auth.service';
@@ -15,7 +14,7 @@ import { RequireAnonGuard } from './guards/require-anon.guard';
 import { RequireUserGuard } from './guards/require-user.guard';
 
 const routes: Routes = [
-  { path: '',  component: HomePageComponent, canActivate: [ InitAuthGuard ] },
+  { path: '',  component: PrivatePageComponent, canActivate: [ RequireUserGuard ] },
   { path: 'login',  component: LoginPageComponent, canActivate: [ RequireAnonGuard ] },
   { path: 'signup',  component: SignupPageComponent, canActivate: [ RequireAnonGuard ] },
   { path: 'private',  component: PrivatePageComponent , canActivate: [ RequireUserGuard ] },
@@ -27,7 +26,7 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     LoginPageComponent,
-    HomePageComponent,
+    // HomePageComponent,
     SignupPageComponent,
     PrivatePageComponent
   ],
